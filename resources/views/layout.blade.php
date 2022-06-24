902a17a4f484b3c57105034bb4cd0787bff8a4d8
unknown <nemexxix@gmail.com>
2022-06-24 14:21:15 +0200

    navbar added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,31 @@
     <title>Projet</title>
 </head>
 <body>
+    <header class="d-flex justify-content-between align-items-center p-3 text-white bg-dark">
+        <div>Projet Heroes</div>
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="me-4">
+                <a href="{{ url('/heroes') }}" class="text-sm text-white text-decoration-none me-2">heroes</a>
+                <a href="{{ url('/genders') }}" class="text-sm text-white text-decoration-none me-2">genders</a>
+                <a href="{{ url('/races') }}" class="text-sm text-white text-decoration-none me-2">races</a>
+                <a href="{{ url('/skills') }}" class="text-sm text-white text-decoration-none me-2">skills</a>
+                <a href="{{ url('/universes') }}" class="text-sm text-white text-decoration-none me-2">universes</a>
+            </div>
+            @if (Route::has('login'))
+                <div class="hidden fixed">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-white text-decoration-none">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-white text-decoration-none">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-white text-decoration-none">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </header>
     <div class="container">
          @yield('content')
     </div>
